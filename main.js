3e7969a7b4734171572cc3463cf30e5b281e5b58
MaxPower15 <max.schnur@gmail.com>
2010-11-30 11:31:32 -0500

    Removing files and setting up redirect to revsystems/jQuery-SelectBox

--- main.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-  $("select").sb({ ddCtx: function() { return $(this).closest("form"); } });
-  $("select[name=example]").append("<option value='8'>New Option</option>").trigger("reload");
-});
